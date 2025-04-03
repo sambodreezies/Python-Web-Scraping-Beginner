2b4b6d670797fe2f4734b6c61da424cee8fe4cbd
sambodreezies <bonafidesamdrees@gmail.com>
2025-04-02 20:15:14 -0400

    Update web_scraping_project.py

--- web_scraping_project.py
+++ web_scraping_project.py
@@ -2,7 +2,6 @@ print("\033c")
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import mechanicalsoup
 import requests
 
 URL = "https://www.python.org/jobs/"
@@ -11,5 +10,29 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 # print(page.text)
 
-# results = soup.find(li = 'listing-company')
-# print(results)
+results = soup.find(id = 'content')
+print(results.prettify())
+
+python_jobs = results.find_all("a", string = lambda text: 'python' in text.lower())
+# print(python_jobs)
+
+python_jobs_filtered = [job.get_text(strip=True) for job in python_jobs]
+
+# python_job_cards = [
+#     a_attribute.parent
+# ]
+
+if True:
+    for job in python_jobs:
+        title_element = job.get_text(strip=True)
+        
+        location_element = job.find_next("span", class_="listing-location")
+        
+        if location_element:
+            location = location_element.get_text(strip=True)
+        else:
+            location = "Location not found"
+        
+        print(title_element)
+
+        print(location)
